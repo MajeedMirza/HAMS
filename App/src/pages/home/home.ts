87d75250d72bd5697721fae33279cdbea1e61187
Hooseon Yim <hoo.yim@gmail.com>
2017-11-24 22:14:02 -0500

    Added Socket IO

--- App/src/pages/home/home.ts
+++ App/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AddNodePage } from '../add-node/add-node'
 import { NodeServiceProvider } from '../../providers/node-service/node-service';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'page-home',
@@ -16,8 +17,23 @@ export class HomePage {
   smoke: any;
   flame: any;
 
+  socket:any;
+
   constructor(public navCtrl: NavController, public nodeServiceProvider: NodeServiceProvider) {
     this.getNodes();
+    this.socket = io(nodeServiceProvider.getServer());
+
+    this.socket.on('values', (node) => {
+      console.log("message", node);
+      // TODO Live update values
+      for (let i in this.nodes) {
+        if (this.nodes[i].id == node.id) {
+          console.log("hi");
+          this.nodes[i].values = node.values;
+        }
+      }
+    });
+
   }
 
   createNode() {
